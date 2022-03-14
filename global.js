31390c67f525f58cf446dd2f0a0799cc9463deb2
Varma <varma@quickfiredigital.com>
2022-03-14 15:28:37 +0000

    some more changes

--- global.js
+++ global.js
@@ -1,2 +1,14 @@
+// to import external modules
+const path = require('path') // to work with directories and files locations or paths
+
+console.log(`The dirname is ${path.dirname(__dirname)}`)
+console.log(`The filename is ${path.basename(__filename)}`)
+
+
 console.log(__dirname);
 console.log(__filename);
+
+
+//process object --> process.argv
+
+console.log(process.argv)
